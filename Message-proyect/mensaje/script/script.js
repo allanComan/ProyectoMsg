1f9d78b2f1d29d179de2a451278c98f976d7e2e6
allanComan <145622315+allanComan@users.noreply.github.com>
2024-01-11 13:19:30 -0600

    Add files msg via upload

---
+++ Message-proyect/mensaje/script/script.js
@@ -0,0 +1,28 @@
+const wrapper = document.querySelector(".content-wraper");
+const question = document.querySelector(".question");
+const gif = document.querySelector(".gif");
+const yesBtn = document.querySelector(".yes-btn");
+const noBtn = document.querySelector(".no-btn");
+
+yesBtn.addEventListener("click", () => {
+  question.textContent = "Perfecto, nos vemos el jueves c:";
+
+  gif.src =
+    "https://i.pinimg.com/originals/ca/42/1d/ca421d4887aad40d311289ca53bce19e.gif";
+
+  yesBtn.style.display = "none";
+  noBtn.style.display = "none";
+});
+
+noBtn.addEventListener("mouseover", () => {
+  const noBtnRect = noBtn.getBoundingClientRect();
+
+  const maxX = window.innerWidth - noBtnRect.width;
+  const maxY = window.innerHeight - noBtnRect.height;
+
+  const randomX = Math.floor(Math.random() * maxX);
+  const randomY = Math.floor(Math.random() * maxY);
+
+  noBtn.style.left = randomX + "px";
+  noBtn.style.top = randomY + "px";
+});
